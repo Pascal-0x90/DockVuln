23acf1d534671f26697dea007c7618acc94cb048
Pascal <pascal@pop-os.localdomain>
2021-01-20 19:24:49 -0700

    added first vulnerable container

---
+++ vulns/php/PHPOI1.php
@@ -0,0 +1,30 @@
+<?php 
+class File {
+  public function flag() {
+    $this->innocent();
+  }
+  public function innocent() {
+    echo "THIS IS NOT THE FLAG :D\n";
+  }
+}
+class GiveFlag extends File {
+    public $offset = 23;
+    public function innocent() {
+    echo file_get_contents("flag.txt");
+    }
+}
+class entry {
+  public function __destruct(){
+    $this->awesome->flag();
+  }
+}
+
+$userin = $_GET['fancy'];
+$unser = unserialize($userin);
+?>
+
+<html>
+    <body>
+        We are deving some things out...
+    </body>
+</html>
